5294b45d70f04cc1b6804322ad327369f06d20eb
Foam <foam@FoamdeMacBook-Air.local>
2025-03-21 12:38:32 +0800

    Add index forgotpassword

--- apps/views.py
+++ apps/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import PasswordResetForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -11,5 +13,23 @@ from django.shortcuts import render
 -------
 """
 
-def login(requests):
-    return  render(requests, 'login.html')
+# 添加主界面
+def index(request):
+    return render(request, 'index.html')
+
+# 添加登录主界面
+def login(request):
+    return render(request, 'login.html')
+
+# 实现忘记密码功能
+def ForgotPassword(request):
+    # if request.method == 'POST':
+    #     form = PasswordResetForm(request.POST)
+    #     if form.is_valid():
+    #         form.save(request=request)
+    #         messages.success(request, '暂未实现邮件发送功能，请联习管理员')
+    #         return redirect(reversed('login'))
+    # else:
+    #     form = PasswordResetForm()
+    # return render(request, 'forgot-password', {'form': form})
+    return render(request, 'forgot-password.html')
